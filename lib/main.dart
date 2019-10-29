2a895b7d59ec66431223df3a745a2df2fc1a97c6
2Mundos <adrianofpinheiro@outlook.com>
2019-10-29 15:40:28 -0300

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+void main() {
+  // Teste do Firebase DataBase
+  // Firestore.instance.collection("testeColection1").document("testeDocument1").setData({"testeData1": "teste"});
+
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
